c24f74e31d234c47ba3a366e9c8bf09e328caf6a
Macbook Marconato <m.lucalmer@gmail.com>
2018-09-26 01:17:28 +0200

    minor fixes

--- data/my_device.r
+++ data/my_device.r
@@ -1,4 +1,7 @@
-get_screen_resolution <- function() {
+plot_to_browser_enabled <- T
+
+get_screen_resolution <- function() 
+{
     return(c(2560, 1440))
     ## or automatically
     ## xdpyinfo_path <- "/opt/X11/bin/xdpyinfo"    
@@ -8,8 +11,10 @@ get_screen_resolution <- function() {
 }
 
 ## returns the screen size in inches
-get_screen_physical_size <- function() {    
-    return(c(24 + 1/4 + 1/8, 13 + 1/32))
+get_screen_physical_size <- function() 
+{
+    ## return(c(24 + 1/4 + 1/8, 13 + 1/32))
+    return(c(30 + 1/4 + 1/8, 17 + 1/32))
     ## return(c(12.8, 7.5))
     ## or automatically, but I preferred to do it manually via a "binary search"
     ## cmd <- sprintf("%s | grep dimensions | perl -pe 's/^.*?(\\([0-9]+x[0-9]+)\ millimeters.*/$1/g' | tr -d '(' | tr 'x' ' '", xdpyinfo_path)
@@ -18,7 +23,8 @@ get_screen_physical_size <- function() {
     ## return(v/25.4)
 }
 
-new_maximized_device <- function() {
+new_maximized_device <- function() 
+{
     resolution <- get_screen_resolution()
     physical_size <- get_screen_physical_size()
     ## for some reasons the ratio of the result I get is different from the ratio of the resolution so I am using only one value
@@ -28,9 +34,32 @@ new_maximized_device <- function() {
     dev.new(width = physical_size[1], height = physical_size[2])
 }
 
-close_all_devices <- function() {
-    if(length(dev.list()) > 0) {
-        for(l in dev.list()) {
+new_browser_plot <- function()
+{
+    if(plot_to_browser_enabled) {
+		# set the path you want
+        png(filename = "~/programming/web/r_image/plots/plot.png", width = 1920, height = 1080, units = "px", pointsize = 12)
+    } else {
+        new_maximized_device()
+    }
+}
+
+end_browser_plot <- function() 
+{
+    if(plot_to_browser_enabled) {
+        dev.off()
+        ## this only work only on my computer, just delete this line
+        system("sh ~/programming/web/r_image/new_plot.sh ~/programming/web/r_image/plots/plot.png")
+        readline(prompt="press <enter> to continue")   
+    }
+}
+
+close_all_devices <- function() 
+{
+    if(length(dev.list()) > 0) 
+{
+        for(l in dev.list()) 
+{
             dev.off(l)   
         }
     }
