58d932877e7dd9368cde707f6bdc4ff51116fc79
Macbook Marconato <m.lucalmer@gmail.com>
2018-11-06 15:37:01 +0100

    working on gene expression profiles

---
+++ data/expression_profiles/hela/gene/geo/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f ./data/sorted.bam ]; then
+    samtools sort -@ 4 -o ./data/sorted.bam ./data/GSM759888_hela.bam
+fi
+if [ ! -f ./data/counts.txt ]; then
+    featureCounts -t exon -g gene_id -a ./data/Homo_sapiens.GRCh37.64.gtf -o ./data/counts.txt ./data/sorted.bam
+fi
+if [ ! -f ./data/gene_expression_profile.tsv ]; then
+    tail -n +3 ./data/counts.txt | cut -f 1,6 > ./data/gene_expression_profile.tsv
+fi
