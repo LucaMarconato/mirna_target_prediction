dae1cb28d21e4aa848f356165e3573f37d53e74c
Macbook Marconato <m.lucalmer@gmail.com>
2018-11-22 19:20:59 +0100

    still working on comparing TargetScan predictions with my predictions

--- data/transfection/parse_targetscan_data.r
+++ data/transfection/parse_targetscan_data.r
@@ -1,3 +1,12 @@
+## to install biomaRt do not use install.packages() but use the following procedure
+## 1) install Bioconductor by using the following code
+##    if (!requireNamespace("BiocManager"))
+##        install.packages("BiocManager")
+##    BiocManager::install()
+## 2) install biomaRt using Bioconductor
+##    BiocManager::install(c("biomaRt"))
+library(biomaRt)
+
 a <- read.table("targetscan_data/elife-05005-supp1-v1.csv", sep = ";", stringsAsFactors = F)
 a <- a[3:nrow(a), ]
 column_names <- as.character(a[1, ])
@@ -6,6 +15,7 @@ colnames(a) <- column_names
 colnames(a)[[1]] <- "refseq_id"
 colnames(a)[[2]] <- "used_in_training"
 colnames(a)[[3]] <- "gene_symbol"
+a$gene_symbol <- toupper(a$gene_symbol)
 a$used_in_training <- as.factor(a$used_in_training)
 
 as.num = function(x, na.strings = "NA") {
@@ -23,7 +33,7 @@ for(i in 4:ncol(a)) {
     a[[i]] <-  as.num(comma_replaced, na.strings = "N/A")
 }
 
-describe_a <- function(a)
+describe_a <- function()
 {
     stopifnot(dim(a)[[2]] == 77)
     print(paste("the matrix has", dim(a)[[1]], "rows and", dim(a)[[2]], "columns"))
@@ -49,4 +59,63 @@ describe_a <- function(a)
     print(paste(rows_with_few_na_count, "rows with less than", few_na, "NAs"))
 }
 
-describe_a(a)
+describe_a()
+
+## for the moment we will not consider different transcript, but for each gene we will choose one representative transcript. For this reason we will now find, for each transcript, the Ensembl id for the corresponding gene
+if(!file.exists("refseq_embl_conversion_table.tsv")) {
+    ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+    refseq_embl_conversion_table <- getBM(attributes = c("refseq_mrna", "ensembl_gene_id", "hgnc_symbol"), filters = "refseq_mrna", values = a$refseq_id, mart = ensembl)
+    refseq_embl_conversion_table$hgnc_symbol <- toupper(refseq_embl_conversion_table$hgnc_symbol)
+    write.table(refseq_embl_conversion_table, file = "refseq_embl_conversion_table.tsv", quote = F, sep = "\t", row.names = F)
+} else {
+    refseq_embl_conversion_table <- read.table("refseq_embl_conversion_table.tsv", header = T, colClasses = c("character", "character", "character"), fill = T)
+}
+
+transcript_without_gene_name_count <- sum(refseq_embl_conversion_table$hgnc_symbol == "")
+## some transcript are originating from genes which do not have a gene name, only an Ensembl id. This is not a problem, I pointed out this just to get some insights of the data
+print(paste("there are", transcript_without_gene_name_count, "transcript originating from a gene with only the Ensembl id and not a gene name"))
+
+number_of_dinstinct_duplicate_genes <- sum((table(refseq_embl_conversion_table$hgnc_symbol) - 1) > 0)
+number_of_non_duplicate_genes <- sum((table(refseq_embl_conversion_table$hgnc_symbol) - 1) == 0)
+print(paste(number_of_non_duplicate_genes, "genes have only one transcript"))
+print(paste(number_of_dinstinct_duplicate_genes, "genes have more than one transcript"))
+
+## integrity check
+matched_transcripts <- match(a$refseq_id, refseq_embl_conversion_table$refseq_mrna)
+not_found_transcripts <- sum(is.na(matched_transcripts))
+found_transcripts <- sum(!is.na(matched_transcripts))
+print(paste(not_found_transcripts, "transcript not found in the refseq/ensembl conversion table"))
+print(paste(found_transcripts, "found"))
+predicted_gene_symbols <- refseq_embl_conversion_table$hgnc_symbol[matched_transcripts]
+na_gene_symbols <- sum(is.na(predicted_gene_symbols))
+if(na_gene_symbols != not_found_transcripts) {
+    print(paste("warning:", na_gene_symbols, "gene symbols are NA,", not_found_transcripts, "expected"))
+}
+rows_differing_by_gene_symbol <- a$gene_symbol != predicted_gene_symbols
+equal_rows_count <- sum(rows_differing_by_gene_symbol == F, na.rm = T)
+differing_rows_count <- sum(rows_differing_by_gene_symbol == T, na.rm = T)
+print(paste("the gene symbol coincides for", equal_rows_count, "rows"))
+print(paste("the gene symbol differs for", differing_rows_count, "rows and is NA for", na_gene_symbols, "rows"))
+
+print_the_differing_rows <- F
+if(print_the_differing_rows) {
+    rows_na_or_differing_by_gene_symbol <- rows_differing_by_gene_symbol | is.na(rows_differing_by_gene_symbol)
+    to_print <- data.frame(refseq_id = a$refseq_id[rows_na_or_differing_by_gene_symbol],
+                           targetscan_gene_symbol = a$gene_symbol[rows_na_or_differing_by_gene_symbol],
+                           predicted_gene_symbols = predicted_gene_symbols[rows_na_or_differing_by_gene_symbol])
+    print(to_print)
+}
+print("the reason behind so many transcript annotated to different genes is probably that I am using a wrong conversion table version, I will fix this. For the moment I will continue working with a subset of the data")
+## for fixing the versions I will have to look at the output of these commands and at the url shown below
+## listDatasets(useMart("ensembl"))
+## listFilters(ensembl)
+## listAttributes(ensembl)
+## various databases maintained in Bioconductor: org.Hs.eg.db
+
+## the matrix with the valid data is called b, I will only consider genes having only one transcript
+b <- a
+b$ensembl_gene_id <- predicted_gene_symbols
+STOPPPED WORKING HEREEEEEEEEEEEE... I NEED TO SELECT THE ROWS WITH UNIQUE GENE SYMBOL
+b$transcripts_with_the_same_gene <- 
+b <- b[!is.na(b$ensembl_gene_id), ]
+print(dim(b))
